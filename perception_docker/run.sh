6fb10ccc8c4be1bf588ccd53252a2ae42cd7db72
Ganapathy Seshadri Cadungude Aiyer <gaiyer@dhcp-172-17-171-9.nvidia.com>
2018-11-13 10:19:19 -0800

    Cleaned up run.sh.

--- perception_docker/run.sh
+++ perception_docker/run.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+DOCKERURL=nvcr.io/nvidia/deepstream_360d:3.0-18.11
 xhost +
-nvidia-docker pull nvcr.io/nvidia/deepstream_360d:3.0-18.11
-nvidia-docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd)/videos/:/root/DeepStream360d_Release/samples/streams/360d_streams -e DISPLAY=$DISPLAY -w /root nvcr.io/nvidia/deepstream_360d:3.0-18.11 
+nvidia-docker pull $DOCKERURL 
+nvidia-docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd)/videos/:/root/DeepStream360d_Release/samples/streams/360d_streams -e DISPLAY=$DISPLAY -w /root $DOCKERURL 
