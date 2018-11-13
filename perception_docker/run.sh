0266a0cf63eb6fce114c91e654bcd4263dc7fbe2
Ganapathy Seshadri Cadungude Aiyer <gaiyer@dhcp-172-17-171-9.nvidia.com>
2018-11-13 09:30:33 -0800

    Updated run script to pull from ngc and updated the setup instructions.

--- perception_docker/run.sh
+++ perception_docker/run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 xhost +
-nvidia-docker pull   gitlab-master.nvidia.com:5005/deepstreamsdk/release_image/deepstream360_release:cl_25208070
-nvidia-docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd)/videos/:/root/DeepStream360d_Release/samples/streams/360d_streams -e DISPLAY=$DISPLAY -w /root  gitlab-master.nvidia.com:5005/deepstreamsdk/release_image/deepstream360_release:cl_25208070 
+nvidia-docker pull nvcr.io/nvidia/deepstream_360d:3.0-18.11
+nvidia-docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd)/videos/:/root/DeepStream360d_Release/samples/streams/360d_streams -e DISPLAY=$DISPLAY -w /root nvcr.io/nvidia/deepstream_360d:3.0-18.11 
