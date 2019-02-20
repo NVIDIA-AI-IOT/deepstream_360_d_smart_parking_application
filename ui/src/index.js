2664519a14513d7bec94bc9cb9a91f9f1af0d7a3
aiyerganapathy <aiyerganapathy@gmail.com>
2019-02-20 07:03:36 -0800

    Changed font to Google Abel.

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './fonts/DINProLight/font.css';
 import './index.css';
 import App from './App';
 import { unregister } from './registerServiceWorker';
