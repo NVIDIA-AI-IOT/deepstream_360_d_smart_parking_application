24366424de6d39ada8e08e772e41c3952e074dc8
Sujit Biswas <sujit.biswas@gmail.com>
2019-11-28 08:19:38 -0800

    update docker url to DS4.0

--- perception_docker/run.sh
+++ perception_docker/run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-DOCKERURL=nvcr.io/nvidia/deepstream_360d:3.0-18.11
+DOCKERURL=nvcr.io/nvidia/deepstream_360d:4.0.1-19.11
 xhost +
 nvidia-docker pull $DOCKERURL 
 nvidia-docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd)/videos/:/root/DeepStream360d_Release/samples/streams/360d_streams -e DISPLAY=$DISPLAY -w /root $DOCKERURL 
