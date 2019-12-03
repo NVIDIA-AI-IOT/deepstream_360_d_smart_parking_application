cf18128a457a10f00cd2b35b422c3a6f67820e6b
nvbrupde <56950268+nvbrupde@users.noreply.github.com>
2019-12-03 13:53:46 +0530

    Update run.sh

--- perception_docker/run.sh
+++ perception_docker/run.sh
@@ -2,4 +2,4 @@
 DOCKERURL=nvcr.io/nvidia/deepstream_360d:4.0.1-19.11
 xhost +
 docker pull $DOCKERURL 
-docker run --gpus all -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -w /root nvcr.io/nvidia/deepstream:4.0.1-19.09-samples
+docker run --gpus all -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -w /root nvcr.io/nvidia/deepstream_360d:4.0.1-19.11
