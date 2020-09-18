c360fe5c8c1711aa674c04cdb0ef04a06d4ab77b
nvbrupde <56950268+nvbrupde@users.noreply.github.com>
2020-09-18 14:28:25 +0530

    Update run.sh
    for DS 5.0 GA

--- perception_docker/run.sh
+++ perception_docker/run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-DOCKERURL=nvcr.io/nvidia/deepstream_360d:4.0.1-19.11
+DOCKERURL=nvcr.io/nvidia/deepstream_360d:5.0-20.08
 xhost +
 docker pull $DOCKERURL 
-docker run --gpus all -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -w /root nvcr.io/nvidia/deepstream_360d:4.0.1-19.11
+docker run --gpus all -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -w /root nvcr.io/nvidia/deepstream_360d:5.0-20.08
